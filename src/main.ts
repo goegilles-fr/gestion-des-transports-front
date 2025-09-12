79144b41df85268ed4f554a342ca5b776a8a1063
Amethyste34 <laurence.amethyste@gmail.com>
2025-09-12 19:08:49 +0200

    Ajout du component Login avec formulaire de validation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(FormsModule)
+  ]
+}).catch(err => console.error(err));
