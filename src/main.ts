c3c95028140eb22d7d42f95096202c77a4c1c061
Amethyste34 <laurence.amethyste@gmail.com>
2025-09-16 21:15:17 +0200

    Authentification JWT complète avec SessionStorage

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { routes } from './app/app.routes';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { App } from './app/app';
+import { routes } from './app/app.routes';
+import { AuthInterceptor } from './app/interceptors/auth';
 
 bootstrapApplication(App, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
-    importProvidersFrom(ReactiveFormsModule)
+    provideHttpClient(withInterceptorsFromDi()),
+    importProvidersFrom(ReactiveFormsModule),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
   ]
-}).catch(err => console.error(err));
+});
